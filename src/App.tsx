deff164eab4d9e8d0cd242d8cad8e6e9b1ce4bfa
Kilesh Maharjan <78723506+kilesh-mhzn@users.noreply.github.com>
2023-11-28 23:42:16 +0545

    UI Layout components: Grid and Flex (#1)
    * flex layout ui component
    
    * grid layout ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import styles from "./App.module.css";
 import { Icon } from "./ui/icon/icon";
+import { Flex } from "./ui/icon/layout/flex";
+import { Grid, GridItem } from "./ui/icon/layout/grid";
 
 function App() {
   return (
     <>
-      <div className={styles.test}>Hello World</div>
-      <Icon name={"add"} />
-      <Icon name={"back-arrow"} />
+      <Flex container justifyContent="space-between">
+        <div className={styles.test}>Hello World</div>
+        <Icon name={"add"} />
+        <Icon name={"back-arrow"} />
+      </Flex>
+
+      <Grid cols={12}>
+        <GridItem colSpan={5}>test</GridItem>
+        <GridItem colSpan={3}>test22</GridItem>
+        <GridItem colSpan={1}>test1</GridItem>
+      </Grid>
     </>
   );
 }
