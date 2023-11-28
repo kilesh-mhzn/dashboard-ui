9babd94c87c452c618ab863bf1c5b8659e6caaea
kilesh maharjan <kileshmhrzn@gmail.com>
2023-11-28 23:22:35 +0545

    flex layout ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./App.module.css";
 import { Icon } from "./ui/icon/icon";
+import { Flex } from "./ui/icon/layout/flex";
 
 function App() {
   return (
     <>
-      <div className={styles.test}>Hello World</div>
-      <Icon name={"add"} />
-      <Icon name={"back-arrow"} />
+      <Flex container justifyContent="space-between">
+        <div className={styles.test}>Hello World</div>
+        <Icon name={"add"} />
+        <Icon name={"back-arrow"} />
+      </Flex>
     </>
   );
 }
