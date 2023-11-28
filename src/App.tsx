a85bad905fa957e0f4650b85ab314778eacdc071
kilesh maharjan <kileshmhrzn@gmail.com>
2023-11-28 23:41:08 +0545

    grid layout ui component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./App.module.css";
 import { Icon } from "./ui/icon/icon";
 import { Flex } from "./ui/icon/layout/flex";
+import { Grid, GridItem } from "./ui/icon/layout/grid";
 
 function App() {
   return (
@@ -11,6 +12,12 @@ function App() {
         <Icon name={"add"} />
         <Icon name={"back-arrow"} />
       </Flex>
+
+      <Grid cols={12}>
+        <GridItem colSpan={5}>test</GridItem>
+        <GridItem colSpan={3}>test22</GridItem>
+        <GridItem colSpan={1}>test1</GridItem>
+      </Grid>
     </>
   );
 }
