531f268e9ec5a13e15f9c0defd3bc6bf1e8f3434
kilesh maharjan <kileshmhrzn@gmail.com>
2023-11-30 22:00:53 +0545

    create side bar component

--- src/components/commons/navbar/navbar.tsx
+++ src/components/commons/navbar/navbar.tsx
@@ -7,19 +7,21 @@ import styles from "./navbar.module.css";
 export const Navbar = () => {
   const { isLightMode, toggleLightMode } = useContext(ThemeContext);
   return (
-    <Flex
-      className={styles["nav-container"]}
-      container
-      justifyContent="space-between"
-      padding={"1rem 4rem"}
-    >
-      <div>Dashboard</div>
+    <nav>
+      <Flex
+        className={styles["nav-container"]}
+        container
+        justifyContent="space-between"
+        padding={"1rem 4rem"}
+      >
+        <div>Dashboard</div>
 
-      <Switch
-        label="Dark mode"
-        checked={isLightMode}
-        onChange={toggleLightMode}
-      />
-    </Flex>
+        <Switch
+          label="Dark mode"
+          checked={isLightMode}
+          onChange={toggleLightMode}
+        />
+      </Flex>
+    </nav>
   );
 };
