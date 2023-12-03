e843460556bf2c3a279a20b7c5829d3ecface977
kilesh maharjan <kileshmhrzn@gmail.com>
2023-12-04 01:03:33 +0545

    create date formatter util

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,24 @@
+import { format } from "date-fns";
+
 export const extractInitials = (fullName: string) => {
   const split = fullName?.split(" ");
   const first = split ? split[0] : ["A", "A"];
   const last = split.reverse()[0];
 
   return `${first[0]}${last[0]}`.toUpperCase();
+};
+
+interface DateFormatterOptions {
+  formatString?: string;
+}
+
+export const formatDate = (
+  timestamp: number | string,
+  options?: DateFormatterOptions
+): string => {
+  const defaultFormatString = "yyyy/MM/dd";
+  const { formatString = defaultFormatString } = options || {};
+
+  const date = new Date(Number(timestamp) * 1000);
+  return format(date, formatString);
 };
