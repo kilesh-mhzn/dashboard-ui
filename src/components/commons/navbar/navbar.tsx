18cb44370bb4d8997a890219161c2f280702debc
kilesh maharjan <kileshmhrzn@gmail.com>
2023-12-04 23:22:00 +0545

    refactor: theme context

--- src/components/commons/navbar/navbar.tsx
+++ src/components/commons/navbar/navbar.tsx
@@ -5,16 +5,17 @@ import React, { useContext } from "react";
 import styles from "./navbar.module.css";
 
 export const Navbar = () => {
-  const { isLightMode, toggleLightMode } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <nav className={styles["nav-container"]}>
       <Flex container justifyContent="space-between" padding={"1rem 4rem"}>
         <div>Dashboard</div>
 
         <Switch
-          label="Dark mode"
-          checked={isLightMode}
-          onChange={toggleLightMode}
+          label={`${theme === "themeDark" ? "Dark" : "Light"}`}
+          checked={theme === "themeDark"}
+          onChange={toggleTheme}
         />
       </Flex>
     </nav>
