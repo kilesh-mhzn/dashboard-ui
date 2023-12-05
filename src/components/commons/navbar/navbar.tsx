e606e723b28d0ebcc01fae69308e24423e39a863
kilesh maharjan <kileshmhrzn@gmail.com>
2023-12-05 19:04:33 +0545

    add icons for representing light and dark mode

--- src/components/commons/navbar/navbar.tsx
+++ src/components/commons/navbar/navbar.tsx
@@ -3,6 +3,7 @@ import { Flex } from "@ui/layout/flex";
 import { Switch } from "@ui/switch/switch";
 import React, { useContext } from "react";
 import styles from "./navbar.module.css";
+import { IconMoon, IconSun } from "@ui/icons/icons";
 
 export const Navbar = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -12,11 +13,14 @@ export const Navbar = () => {
       <Flex container justifyContent="space-between" padding={"1rem 4rem"}>
         <div>Dashboard</div>
 
-        <Switch
-          label={`${theme === "themeDark" ? "Dark" : "Light"}`}
-          checked={theme === "themeDark"}
-          onChange={toggleTheme}
-        />
+        <Flex container alignItems="center" gap={"1rem"}>
+          <Switch checked={theme === "themeDark"} onChange={toggleTheme} />
+          {theme === "themeDark" ? (
+            <IconMoon color="#f29941" />
+          ) : (
+            <IconSun color="#f29941" />
+          )}
+        </Flex>
       </Flex>
     </nav>
   );
